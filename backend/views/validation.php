bb3c8cdf8bcbfea11eb5bb25fd0a7c817b4f7048
slim20000 <76734226+slim20000@users.noreply.github.com>
2021-01-05 16:45:53 +0100

    Update validation.php

--- backend/views/validation.php
+++ backend/views/validation.php
@@ -15,22 +15,22 @@ SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 
 <!-- Bootstrap Core CSS -->
-<link href="style/css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
 
 <!-- Custom CSS -->
-<link href="style/css/style.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
 
 <!-- font-awesome icons CSS -->
-<link href="style/css/font-awesome.css" rel="stylesheet"> 
+<link href="css/font-awesome.css" rel="stylesheet"> 
 <!-- //font-awesome icons CSS -->
 
  <!-- side nav css file -->
- <link href='style/css/SidebarNav.min.css' media='all' rel='stylesheet' type='text/css'/>
+ <link href='css/SidebarNav.min.css' media='all' rel='stylesheet' type='text/css'/>
  <!-- side nav css file -->
  
  <!-- js-->
-<script src="style/js/jquery-1.11.1.min.js"></script>
-<script src="style/js/modernizr.custom.js"></script>
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/modernizr.custom.js"></script>
  <!--//js-->
  
 <!--webfonts-->
@@ -38,9 +38,9 @@ SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony
 <!--//webfonts--> 
 
 <!-- Metis Menu -->
-<script src="style/js/metisMenu.min.js"></script>
-<script src="style/js/custom.js"></script>
-<link href="style/css/custom.css" rel="stylesheet">
+<script src="js/metisMenu.min.js"></script>
+<script src="js/custom.js"></script>
+<link href="css/custom.css" rel="stylesheet">
 <!--//Metis Menu -->
 
 </head> 
@@ -55,140 +55,22 @@ SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony
 		<!-- main content start-->
 		<div id="page-wrapper">
 			<div class="main-page">
-				<div class="forms validation">
-					<h2 class="title1">Validation Forms :</h2>
-					<div class="form-three widget-shadow">
-						<div data-example-id="form-validation-states"> 
-							<form> <div class="form-group has-success"> <label class="control-label" for="inputSuccess1">Input with success</label> <input type="text" class="form-control" id="inputSuccess1" aria-describedby="helpBlock2"> <span id="helpBlock2" class="help-block">A block of help text that breaks onto a new line and may extend beyond one line.</span> </div> <div class="form-group has-warning"> <label class="control-label" for="inputWarning1">Input with warning</label> <input type="text" class="form-control" id="inputWarning1"> </div> <div class="form-group has-error"> <label class="control-label" for="inputError1">Input with error</label> <input type="text" class="form-control" id="inputError1"> </div> <div class="has-success"> <div class="checkbox"> <label> <input type="checkbox" id="checkboxSuccess" value="option1"> Checkbox with success </label> </div> </div> <div class="has-warning"> <div class="checkbox"> <label> <input type="checkbox" id="checkboxWarning" value="option1"> Checkbox with warning </label> </div> </div> <div class="has-error"> <div class="checkbox"> <label> <input type="checkbox" id="checkboxError" value="option1"> Checkbox with error </label> </div> </div> </form> 
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-6 validation-grids widget-shadow" data-example-id="basic-forms"> 
-							<div class="form-title">
-								<h4>Register Form :</h4>
-							</div>
-							<div class="form-body">
-								<form data-toggle="validator">
-									<div class="form-group">
-										<input type="text" class="form-control" id="inputName" placeholder="Username" required>
-									</div>
-									<div class="form-group has-feedback">
-										<input type="email" class="form-control" id="inputEmail" placeholder="Email" data-error="Bruh, that email address is invalid" required>
-										<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
-										<span class="help-block with-errors">Please enter a valid email address</span>
-									</div>
-									<div class="form-group">
-									  <input type="password" data-toggle="validator" data-minlength="6" class="form-control" id="inputPassword" placeholder="Password" required>
-									  <span class="help-block">Minimum of 6 characters</span>
-									</div>
-									<div class="form-group">
-									  <input type="password" class="form-control" id="inputPasswordConfirm" data-match="#inputPassword" data-match-error="Whoops, these don't match" placeholder="Confirm password" required>
-									  <div class="help-block with-errors"></div>
-									</div>
-									<div class="form-group">
-										<div class="radio">
-											<label>
-											  <input type="radio" name="gender" required>
-											  Female
-											</label>
-										</div>
-										<div class="radio">
-											<label>
-											<input type="radio" name="gender" required>
-											Male
-											</label>
-										</div>
-									</div>
-									<div class="form-group">
-										<div class="checkbox">
-											<label>
-												<input type="checkbox" id="terms" data-error="Before you wreck yourself" required>
-												I have read and accept terms of use.
-											</label>
-											<div class="help-block with-errors"></div>
-										</div>
-									</div>
-									<div class="form-group">
-										<button type="submit" class="btn btn-primary disabled">Submit</button>
-									</div>
-								</form>
-							</div>
-						</div>
-						<div class="col-md-6 validation-grids validation-grids-right">
-							<div class="widget-shadow" data-example-id="basic-forms"> 
-								<div class="form-title">
-									<h4>Login form :</h4>
-								</div>
-								<div class="form-body">
-									<form data-toggle="validator">
-										<div class="form-group has-feedback">
-											<input type="email" class="form-control" id="inputEmail" placeholder="Enter Your Email" data-error="Bruh, that email address is invalid" required>
-											<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
-										</div>
-										<div class="form-group">
-											<input type="password" data-toggle="validator" data-minlength="6" class="form-control" id="inputPassword" placeholder="Password" required>
-										</div>
-										<div class="bottom">
-											<div class="form-group">
-												<div class="checkbox">
-													<label>
-														<input type="checkbox" id="terms" data-error="Before you wreck yourself" required>
-														Remember me
-													</label>
-													<div class="help-block with-errors"></div>
-												</div>
-											</div>
-											<div class="form-group">
-												<button type="submit" class="btn btn-primary disabled">Login</button>
-											</div>
-											<div class="clearfix"> </div>
-										</div>
-									</form>
-								</div>
-							</div>
-							<div class="inline-form widget-shadow">
-								<div class="form-title">
-									<h4>Recover form :</h4>
-								</div>
-								<div class="form-body">
-									<form data-toggle="validator">
-										<div class="form-group has-feedback">
-											<input type="email" class="form-control" id="inputEmail" placeholder="Email" data-error="Bruh, that email address is invalid" required>
-											<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
-										</div>
-										<div class="form-group">
-											<input type="text" data-toggle="validator" data-minlength="6" class="form-control" id="inputPassword" placeholder="Enter your phone number" required>
-										</div>
-										<div class="bottom">
-											<div class="form-group">
-												<button type="submit" class="btn btn-primary disabled">Login</button>
-											</div>
-										</div>
-									</form>
-								</div>
-							</div>
-						</div>
-						<div class="clearfix"> </div>	
-					</div>
-				</div>
-			</div>
+				
 		</div>
 		<!--footer-->
-		<div class="footer">
-		   <p>&copy; 2018 Glance Design Dashboard. All Rights Reserved | Design by <a href="https://w3layouts.com/" target="_blank">w3layouts</a></p>
-	   </div>
+		
         <!--//footer-->
 	</div>
 	
 	<!-- side nav js -->
-	<script src='style/js/SidebarNav.min.js' type='text/javascript'></script>
+	<script src='js/SidebarNav.min.js' type='text/javascript'></script>
 	<script>
       $('.sidebar-menu').SidebarNav()
     </script>
 	<!-- //side nav js -->
 	
 	<!-- Classie --><!-- for toggle left push menu script -->
-		<script src="style/js/classie.js"></script>
+		<script src="js/classie.js"></script>
 		<script>
 			var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
 				showLeftPush = document.getElementById( 'showLeftPush' ),
@@ -210,15 +92,15 @@ SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony
 	<!-- //Classie --><!-- //for toggle left push menu script -->
 	
 	<!--scrolling js-->
-	<script src="style/js/jquery.nicescroll.js"></script>
-	<script src="style/js/scripts.js"></script>
+	<script src="js/jquery.nicescroll.js"></script>
+	<script src="js/scripts.js"></script>
 	<!--//scrolling js-->
 	
 	<!-- Bootstrap Core JavaScript -->
-	<script src="style/js/bootstrap.js"> </script>
+	<script src="js/bootstrap.js"> </script>
 	
 	<!--validator js-->
-	<script src="style/js/validator.min.js"></script>
+	<script src="js/validator.min.js"></script>
 	<!--//validator js-->
 	
 </body>
