636166f84b5a570ac6eece984df6baedfa2fc12f
slim20000 <76734226+slim20000@users.noreply.github.com>
2021-01-06 15:09:37 +0100

    Add files via upload

---
+++ image-upload-php-and-mysql-main/planning/view.php
@@ -0,0 +1,44 @@
+<?php include "db_conn.php"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>View</title>
+	<style>
+		body {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			flex-wrap: wrap;
+			min-height: 100vh;
+		}
+		.alb {
+			width: 7200px;
+			height: 600px;
+			padding:1px;
+		}
+		.alb img {
+			width: 100%;
+			height: 100%;
+		}
+		a {
+			text-decoration: none;
+			color: black;
+		}
+	</style>
+</head>
+<body>
+     <a href="http://localhost/webProject-main/CyberGym/class-details.php">PLANNING</a>
+     <?php 
+          $sql = "SELECT * FROM images ORDER BY id DESC";
+          $res = mysqli_query($conn,  $sql);
+
+          if (mysqli_num_rows($res) > 0) {
+          	while ($images = mysqli_fetch_assoc($res)) {  ?>
+             
+             <div class="alb">
+             	<img src="uploads/<?=$images['image_url']?>">
+             </div>
+          		
+    <?php } }?>
+</body>
+</html>
